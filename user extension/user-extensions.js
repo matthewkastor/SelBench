8d5d1aa11eda49dc5708c2dd620aa0e8a89cd7f8
Matthew Kastor <matthewkastor@gmail.com>
2014-12-20 19:05:08 -0500

    augmentation code moved to config.js
    This pulls all the server augmentation code out of the firefox
    extension. The only bits that are left in the FF extension are where
    we'd perform an action on a different object in the server.
    
    This passes tests in both the IDE and on the server in the same way.

--- user extension/user-extensions.js
+++ user extension/user-extensions.js
@@ -1,6 +1,91 @@
-this.onServer = true;
+/*jslint
+indent:2,
+maxerr:500,
+plusplus:true
+ */
+/*globals
+selblocks,
+HtmlRunnerTestLoop,
+Selenium,
+htmlTestRunner
+ */
+var globalContext = this;
+globalContext.onServer = globalContext.onServer || true;
+globalContext.serverPatchApplied = globalContext.serverPatchApplied || false;
+
+function seleniumResetInterceptor() {
+  "use strict";
+  var old_reset;
+  old_reset = Selenium.prototype.reset;
+
+  Selenium.prototype.reset = function () {
+    function map_list(list, for_func, if_func) {
+      var i,
+        x,
+        mapped_list = [];
+      for (i = 0; i < list.length; ++i) {
+        x = list[i];
+        // AJS: putaquiupariu
+        if (undefined === if_func || if_func(i, x)) {
+          mapped_list.push(for_func(i, x));
+        }
+      }
+      return mapped_list;
+    }
+
+    /*jslint unparam:true */
+    function importCommands(i, x) {
+      var b = x.getCommand();
+      if (x.hasOwnProperty('trElement')) {
+        b.type = "command";
+      } else {
+        b.type = "comment";
+      }
+      return b;
+    }
+    /*jslint unparam:false */
+
+    old_reset.call(this);
+    // if htmlTestRunner is defined...
+    if (!(htmlTestRunner === undefined || htmlTestRunner === null)) {
+      //TODO: map commands to real types instead of faking it
+      htmlTestRunner.currentTest.commands = map_list(htmlTestRunner.currentTest.htmlTestCase.getCommandRows(), importCommands);
+      // AJS: initializes private testCase (closure) to point to htmlTestRunner.currentTest (public testCase is not available under Core).
+      globalContext.testCase = htmlTestRunner.currentTest;
+      // the debugContext isn't there, but redirecting to the testCase seems to work.
+      globalContext.testCase.debugContext = globalContext.testCase;
+    }
+  };
+}
+
+function patchServerEnvironment() {
+  "use strict";
+
+  if (globalContext.scriptServerPatchApplied !== true) {
+    globalContext.testCase = {};
+    HtmlRunnerTestLoop.prototype.old_initialize = HtmlRunnerTestLoop.prototype.initialize;
+    HtmlRunnerTestLoop.prototype.initialize = function (htmlTestCase, metrics, seleniumCommandFactory) {
+      this.old_initialize(htmlTestCase, metrics, seleniumCommandFactory);
+      this.commands = [];
+    };
+  }
+  seleniumResetInterceptor();
+  globalContext.serverPatchApplied = true;
+}
+
+// There's an option to use user-extensions.js in the IDE
+// but it runs the tests against the webdriver backed selenium
+// which is a different API than the one that tests are written against...
+if ((globalContext.onServer === true) && (globalContext.serverPatchApplied === false)) {
+  patchServerEnvironment();
+}
 // SelBench name-space
 var selbench = { name: "SelBench" };
+
+// I don't want to redeclare the variable when the user-extension is generated
+// I don't want the scripts to blow up when the firefox extension is used.
+// This is an alias on the global scope that references the global scope.
+globalContext = this;
 
 (function($$){
 	
@@ -207,20 +292,8 @@ var selbench = { name: "SelBench" };
 function $w() { return selenium.browserbot.getCurrentWindow(); }
 function $d() { return selenium.browserbot.getDocument(); }
 
-var globalContext = this;
 // selbench name-space
 (function($$){
-  if(globalContext.onServer === true && globalContext.scriptServerPatchApplied !== true) {
-    globalContext.testCase = {};
-    HtmlRunnerTestLoop.prototype.old_initialize = HtmlRunnerTestLoop.prototype.initialize;
-    HtmlRunnerTestLoop.prototype.initialize = function (htmlTestCase, metrics, seleniumCommandFactory) {
-      LOG.info("SelBench: wrapper to HtmlRunnerTestLoop.initialize.");
-      this.old_initialize(htmlTestCase, metrics, seleniumCommandFactory);
-      this.commands = [];
-    };
-    globalContext.scriptServerPatchApplied = true;
-  }
-
   function evalWithVars(expr) {
     return eval("with (storedVars) {" + expr + "}");
   }
@@ -235,43 +308,6 @@ var globalContext = this;
       orig_reset.call(this);
       // called before each: execute a single command / run a testcase / run each testcase in a testsuite
       $$.LOG.debug("In SelBench tail intercept :: selenium.reset()");
-      if (globalContext.onServer === true && globalContext.selblocks !== true) {
-        //globalContext.scriptInterceptsSeleniumReset = true;
-        function map_list(list, for_func, if_func) {
-          var i,
-          x,
-          mapped_list = [];
-          LOG.info("SelBench: maplist to get commandrows.");
-          for (i = 0; i < list.length; ++i) {
-            x = list[i];
-            // AJS: putaquiupariu
-            if (null == if_func || if_func(i, x)) {
-              mapped_list.push(for_func(i, x));
-            }
-          }
-          return mapped_list;
-        }
-        if (htmlTestRunner == undefined
-           || htmlTestRunner == null) {
-          LOG.info("Selbench: Selenium reset pre htmlTestRunner instatiation ");
-        } else {
-          LOG.info("Selbench: Selenium reset after instatiation of htmlTestRunner.currentTest.htmlTestCase:" + htmlTestRunner.currentTest.htmlTestCase);
-          //TODO: map commands to real types instead of faking it
-          htmlTestRunner.currentTest.commands = map_list(htmlTestRunner.currentTest.htmlTestCase.getCommandRows(), function (i, x) {
-              var b = x.getCommand();
-              if (x.hasOwnProperty('trElement')) {
-                b.type = "command";
-              } else {
-                b.type = "comment";
-              }
-              return b;
-            });
-          // AJS: initializes private testCase (closure) to point to htmlTestRunner.currentTest (public testCase is not available under Core).
-          testCase = htmlTestRunner.currentTest;
-          // the debugContext isn't there, but redirecting to the testCase seems to work.
-          testCase.debugContext = testCase;
-        }
-      }
       try {
         compileSelbenchCommands();
       }
