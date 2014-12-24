ee2f1ca986540139c31751a9b1837d236c9abe4b
Matthew Kastor <matthewkastor@gmail.com>
2014-12-23 23:48:59 -0500

    adds launcher for test / debug on server
    This is the same launcher as in selblocks, but configured for this
    project. If you compare the two files they're only different in a few of
    the variable settings at the top of the file.

--- build/createSelbenchUserExtensions.cmd
+++ build/createSelbenchUserExtensions.cmd
@@ -1,10 +1,10 @@
 @echo off
-
+SETLOCAL
 SET extensionsDir=%~dp0..\selbench-fx-xpi\chrome\content\extensions
 SET userExtensionDir=%~dp0..\user extension
 
 SET copylist="%userExtensionDir%\scripts\config.js"+"%extensionsDir%\name-space.js"+"%extensionsDir%\logger.js"+"%extensionsDir%\function-intercepting.js"+"%extensionsDir%\selenium-executionloop-handleAsExpectError.js"+"%extensionsDir%\selbench.js"
 
 copy %copylist% /B "%userExtensionDir%\user-extensions.js" /B
-
-
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
